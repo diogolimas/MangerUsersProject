3ec91d92fa133bd2c29c571178bd1b8055dc7d7a
diogo <diogo.libras43@gmail.com>
2021-03-06 18:25:18 -0300

    adding one solution and mapping

--- src/Manager.Infra/Interfaces/IUserRepository.cs
+++ src/Manager.Infra/Interfaces/IUserRepository.cs
@@ -1,11 +1,13 @@
+using System.Collection.Generic;
+using System.Threading.Tasks;
+using Manager.Domain.Entities;
+    
 namespace Manager.Infra.Interfaces{
     public interface IUserRepository: IBaseRepository<User>
     
     {
-
         Task<User> GetNyEmail (string email);
         Task<List<User>> SearchEmail(string email);
-
         Task<List<User>> SearchByName(string name);
 
     }
--- src/Manager.Infra/Repositories/BaseRepository.cs
+++ src/Manager.Infra/Repositories/BaseRepository.cs
@@ -1,3 +1,10 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Manager.Domain.Entities;
+using Manager.Infra.Interfaces;
+using Manager.Infra.Context;
+
 namespace Manager.Infra.Repositories
 {
     public class BaseRepository<T>: IBaseRepository<T> where  T: BaseRepository
@@ -18,7 +25,41 @@ namespace Manager.Infra.Repositories
 
             return obj;
         }
+
+        public virtual async Task<T> Update(T obj)
+        {
+            _context.Entry(obj).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return obj;
+        }
+
+        public virtual async Task Remove(long id)
+        {
+            var obj = await Get(id);
+            if(obj != null){
+                _context.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+        public virtual async Task<T> Get(long id)
+        {
+            var obj = await _context.Set<T>()
+                                    .AsNoTracking()
+                                    .Whererew(x => x.Id == id)
+                                    .ToListAsync();
+            return obj.FirstOrDefault();
+        }
+
+
+        public virtual async Task<List<T>> Get()
+        {
+            return await _context.Set<T>()
+                                .AsNoTracking()
+                                .ToListAsync();
+        }
     }
 
-    
 }
